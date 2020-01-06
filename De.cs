df415723a969829578af5018014301c0c0a82dc6
aurorele13 <59084068+aurorele13@users.noreply.github.com>
2020-01-06 16:06:37 +0100

    Add files via upload

---
+++ De.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPPetitJeu
+{
+    class De
+    {
+        public static int LancerLeDe()
+        {
+            
+            Random random = new Random();
+            int valeurDe = random.Next(1, 7);
+
+            return valeurDe;
+        }
+    }
+}
---
+++ MonstreFacile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPPetitJeu
+{
+    class MonstreFacile
+    {
+        public bool StatusAlive { get;}
+
+        public virtual void Attaquer(Joueur joueur)
+        {
+            int déJoueur = De.LancerLeDe();
+            if (déJoueur > 2)
+                joueur.SubirDegats(10);
+        }
+    }
+}
