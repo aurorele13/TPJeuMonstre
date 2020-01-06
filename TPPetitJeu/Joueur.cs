6971b39caf7a7e9d4d8d69cf19ddb61d8849346e
Aurore <aurorele13@gmail.com>
2020-01-06 16:26:24 +0100

    test

---
+++ TPPetitJeu/Joueur.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPPetitJeu
+{
+    class Joueur
+    {
+        private int _points;
+        private int _pv;
+        private int _nbMonstreDiff;
+        private int _nbMonstreFacile;
+
+        public int NbDiff
+        {
+            get { return _nbMonstreDiff; }
+        }
+
+
+        public int NbFacile { get { return _nbMonstreFacile; } }
+
+
+        public int PV { get { return _pv; } }
+        public bool StatusAlive
+        {
+            get
+            {
+                if (PV > 0)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        public int Points
+        {
+            get { return _points; }
+        }
+        public Joueur()
+        {
+            _pv = 150;
+            _points = 0;
+            _nbMonstreDiff = 0;
+            _nbMonstreFacile = 0;
+        }
+        public void Attaquer(MonstreFacile monstre)
+        {
+
+            if (monstre is MonstreDifficile)
+            {
+                _points += 2;
+                _nbMonstreDiff++;
+            }
+            else
+            {
+                _points++;
+                _nbMonstreFacile++;
+            }
+        }
+
+
+        public void SubirDegats(int dégatsSubis)
+        {
+            _pv = _pv - dégatsSubis;
+        }
+    }
+}
---
+++ TPPetitJeu/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TPPetitJeu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Joueur joueur = new Joueur();
+            MonstreFacile monstre = new MonstreFacile();
+            MonstreDifficile monstreDiff = new MonstreDifficile();
+            Random random = new Random();
+
+            while (joueur.StatusAlive)
+            { 
+            int tirageMonstre = random.Next(1, 3);
+
+            if (tirageMonstre == 2)
+                monstre = monstreDiff;
+
+            int déJoueur = De.LancerLeDe();
+            int déMonstre = De.LancerLeDe();
+            
+            if (déJoueur>=déMonstre)
+            joueur.Attaquer(monstre);
+            else
+            monstre.Attaquer(joueur);
+            
+           
+            }
+            Console.WriteLine($" Snif, vous êtes mort...\n Bravo!! Vous avez tué {joueur.NbFacile} monstres faciles et {joueur.NbDiff} monstres difficiles." +
+               $"Vous avez donc obtenu {joueur.Points} points.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
